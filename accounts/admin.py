2e2fec57cbaa460c09ef4597e3d9c733b3d3f503
Suresh Thagunna <suresh.thagunna77@gmail.com>
2020-06-14 09:22:02 +0545

    create MainAccount and SecondaryAccount models register it to the admin make migrations

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import MainAccount, SecondaryAccount
 # Register your models here.
+
+admin.site.register(MainAccount)
+admin.site.register(SecondaryAccount)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class MainAccount(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255)
+    code = models.FloatField(max_length=255)
+
+class SecondaryAccount(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now_add=True)
+    main_account = models.ForeignKey(MainAccount, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    code = models.FloatField(max_length=255)
