9ec41f448d018cea07b73079194490e5611d86cc
Suresh Thagunna <suresh.thagunna77@gmail.com>
2020-06-19 16:33:22 +0545

    set template dirs in the project settings create a base template create and render template for data_entry view

--- data_entry/views.py
+++ data_entry/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
 def data_entry(request):
-    return HttpResponse("data-entry view")
+    template = 'data_entry/data_entry.html'
+
+    return render(request, template)
