b6433f2a8efafaee13e3744b2cc6eb9dd1b35203
Suresh Thagunna <suresh.thagunna77@gmail.com>
2020-06-22 16:37:39 +0545

    create data_entry view and template create data_entry model form make secondary_account field of the form dependent register data_entry app models to the admin

--- data_entry/admin.py
+++ data_entry/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import (EntryBundle, Entry)
 # Register your models here.
+
+admin.site.register(EntryBundle)
+admin.site.register(Entry)
+
--- data_entry/views.py
+++ data_entry/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
+from .forms import DataEntryForm
+from accounts.models import SecondaryAccount
+
 # Create your views here.
 
 def data_entry(request):
     template = 'data_entry/data_entry.html'
 
-    return render(request, template)
+    if request.method == 'POST':
+        form = DataEntryForm(request.POST)
+        if form.is_valid():
+            entry = form.save(commit=False)
+            print(entry.main_account)
+            print(entry.secondary_account)
+            print(entry.personal_account)
+            print(entry.amount)
+    else:
+        form = DataEntryForm
+
+    context = {
+            'form': form,
+            }
+
+    return render(request, template, context)
+
+def load_secondary_accounts(request):
+    template = 'data_entry/secondary_account_dropdown_list_options.html'
+
+    main_account_id = request.GET.get('main_account')
+    secondary_accounts = SecondaryAccount.objects.filter(main_account_id = main_account_id).order_by('code')
+
+    context = {
+            'secondary_accounts': secondary_accounts,
+            }
+
+    return render(request, template, context)
