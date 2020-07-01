b3683a4a76099dd9b0d4bf3d3f294b123f19f113
Suresh Thagunna <suresh.thagunna77@gmail.com>
2020-07-01 10:35:59 +0545

    remove max_length attribute from code field of accounting app models return human friedly string representation of account objects

--- accounts/models.py
+++ accounts/models.py
@@ -6,19 +6,27 @@ class MainAccount(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255)
-    code = models.FloatField(max_length=255)
+    code = models.FloatField()
+
+    def __str__(self):
+        return str(self.code) + " / " + self.name
 
 class SecondaryAccount(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now_add=True)
     main_account = models.ForeignKey(MainAccount, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    code = models.FloatField(max_length=255)
+    code = models.FloatField()
+
+    def __str__(self):
+        return str(self.code) + " / " + self.name
 
 class PersonalAccount(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255)
-    code = models.FloatField(max_length=255)
     address = models.CharField(max_length=255)
+    code = models.FloatField()
 
+    def __str__(self):
+        return str(self.code) + " / " + self.name
